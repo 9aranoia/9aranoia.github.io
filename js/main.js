5d819fadb8c394228f89a1e695e69325f811f89b
9aranoia <aionara999@gmail.com>
2022-06-14 11:40:43 -0300

    update all pages, add responsive footer and fix errors

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+    
+
+    });
+
